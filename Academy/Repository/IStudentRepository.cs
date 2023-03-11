bfc1056a6990d092d850d004ed11144ed94400ee
Muhammed Roshan <mroshan74@gmail.com>
2023-03-12 00:08:16 +0530

    add repository pattern, for mock and tests

---
+++ Academy/Repository/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Academy.Models;
+
+namespace Academy.Repository;
+
+public interface IStudentRepository
+{
+    Task<Guid> CreateStudentAsync(Student student);
+    Task UpdateStudentAsync(Student student);
+    Task<Student?> GetStudentByIdAsync(Guid studentId);
+    Task<List<Student>> GetStudentsAsync();
+    Task DeleteStudentAsync(int studentId);
+}
---
+++ Academy/Repository/StudentRepository.cs
@@ -0,0 +1,51 @@
+﻿using Academy.Database;
+using Academy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academy.Repository;
+
+public class StudentRepository : IStudentRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public StudentRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<Guid> CreateStudentAsync(Student student)
+    {
+        _dbContext.Students.Add(student);
+        await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+        return student.Id;
+    }
+
+    public async Task UpdateStudentAsync(Student student)
+    {
+        _dbContext.Students.Attach(student);
+        _dbContext.Entry(student).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public async Task<Student?> GetStudentByIdAsync(Guid studentId)
+    {
+        return await _dbContext.Students.FindAsync(studentId).ConfigureAwait(false);
+    }
+
+    public async Task<List<Student>> GetStudentsAsync()
+    {
+        return await _dbContext.Students.ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task DeleteStudentAsync(int studentId)
+    {
+        var student = await _dbContext.Students.FindAsync(studentId).ConfigureAwait(false);
+        if (student != null)
+        {
+            _dbContext.Students.Remove(student);
+            await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        throw new InvalidOperationException("Id not found !");
+    }
+}
